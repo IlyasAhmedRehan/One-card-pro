e2e82acd63845209a225b617999f897ae26cf354
IlyasAhmedRehan <rehanilyasahmed@gmail.com>
2021-12-08 00:31:56 +0500

    First UI Check In

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    //Initialize tooltips
+    $('#createCard .nav-tabs > li a[title]').tooltip();
+
+    //Wizard
+    $('#createCard a[data-toggle="tab"]').on('show.bs.tab', function(e) {
+
+        var $target = $(e.target);
+
+        if ($target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $("#createCard .next-step").click(function(e) {
+
+        var $active = $('#createCard .wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $("#createCard .prev-step").click(function(e) {
+
+        var $active = $('#createCard .wizard .nav-tabs li.active');
+        prevTab($active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('#createCard a[data-toggle="tab"]').click();
+}
+
+function prevTab(elem) {
+    $(elem).prev().find('#createCard a[data-toggle="tab"]').click();
+}
